2889de7ea510d82cb07eba8c6587934250c70ef3
zunaedL <mdzunaed4@gmail.com>
2024-06-14 20:57:45 +0600

    statement

--- js/test.js
+++ js/test.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+const a = 7;
+const b = 8;
+
+if (a > b) {
+    console.log("a is greater than b");
+} else if (a == b) {
+    console.log("a equal b");
+} else {
+    console.log("b is greater than a");
+}
+
+console.log(typeof a);
