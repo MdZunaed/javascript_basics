30674caf2ff88ad947adf53464697902320806e2
zunaedL <mdzunaed4@gmail.com>
2024-06-15 22:55:55 +0600

    object

--- js/test.js
+++ js/test.js
@@ -1,14 +1,38 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-const a = 7;
-const b = 8;
+//////////////// variable
 
-if (a > b) {
-    console.log("a is greater than b");
-} else if (a == b) {
-    console.log("a equal b");
-} else {
-    console.log("b is greater than a");
-}
+// const a = 7;
+// const b = 8;
 
-console.log(typeof a);
+
+//////////////// statement
+
+// if (a > b) {
+//     console.log("a is greater than b");
+// } else if (a == b) {
+//     console.log("a equal b");
+// } else {
+//     console.log("b is greater than a");
+// }
+
+// console.log(typeof a);
+
+//////////////// object
+
+// let Car = {
+//     Brand: "Supra",
+//     model: "MK5",
+//     power: 680,
+//     isSuperCar: true,
+//     startCar: function () {
+//         console.log(this.Brand + ' ' + this.model + ' starting at ' + this.power + ' horse power.');
+//     }
+
+// }
+
+// let supra = Car;
+// console.log(supra.Brand);
+// supra.startCar();
+
+
