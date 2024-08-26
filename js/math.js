4ebe2574e8bf69ea77a633e76946bcb64aaeefaa
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2024-08-26 14:45:30 +0600

    window, dom

---
+++ js/math.js
@@ -0,0 +1,14 @@
+let unfigureNumer = 9.42;
+let someNumbers = [42, 7, 68, 12, 40, 9, 35];
+
+let randomNumber = Math.random();
+let figuredNumber = Math.round(unfigureNumer);
+let lowestNumber = Math.min(42, 7, 68, 12, 40, 9, 35);
+let highestNumber = Math.max(42, 7, 68, 12, 40, 9, 35);
+
+
+
+console.log(`random number between 0 & 1: ${randomNumber}`);
+console.log(`figured number is: ${figuredNumber}`);
+console.log(`lowest number is: ${lowestNumber}`);
+console.log(`highest number is: ${highestNumber}`);
