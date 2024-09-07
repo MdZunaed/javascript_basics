5dc0f69a93666beb7c8c3dd182e0bed2c18e4870
zunaedL <mdzunaed4@gmail.com>
2024-09-07 21:55:50 +0600

    object, array, loop

--- ES6/main.js
+++ ES6/main.js
@@ -1,7 +1,30 @@
 //console.log("Hello World");
 
-let oldArray = [1,2,4];
-let newArray = [3,5,6];
-let combinedArray = [...oldArray,...newArray, 7,8];
+let oldArray = [1, 2, 4];
+let newArray = [3, 5, 6];
+let combinedArray = [...oldArray, ...newArray, 7, 8];
 
-console.log(combinedArray);
+//console.log(combinedArray);
+
+function sumValues(...numbers) {
+    let sum = 0;
+    for (let i of numbers) {
+        sum += i;
+    }
+    console.log("Sum: " + sum);
+}
+
+//sumValues(15,16,9);
+
+let dynamicFunction = function (value) {
+    return value;
+}
+
+//console.log(dynamicFunction("This is dynamic function"));
+
+let laptop = { cpu: "5600g", ram: 8, ssd: 256, gaming: false };
+for (property in laptop) {
+    //console.log(property);
+    //console.log(property + ':' + laptop[property]);
+}
+
