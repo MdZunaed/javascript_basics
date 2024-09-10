a32a2c74f0e0e2735a440b347115c9b6a85059ac
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2024-09-10 13:42:34 +0600

    class & inheritance

---
+++ ES6/class.js
@@ -0,0 +1,38 @@
+class Computer {
+    brand = "AMD";
+    static motherboardModel = "Gigabyte B450m Auros Elite";
+
+    constructor({ cpu, ram, ssd, gpu }) {
+        this.cpu = cpu;
+        this.gpu = gpu;
+        this.ram = ram;
+        this.ssd = ssd;
+    };
+
+
+    runComputer() {
+        console.log(`Starting PC with ${this.cpu}, ${this.gpu}, ${this.ram} ram, & ${this.ssd} ssd`);
+    }
+
+    showBrandName() {
+        console.log(this.brand);
+    }
+}
+
+let zunaedsComputer = new Computer({ cpu: "Ryzen 5500", gpu: "GTX 1650", ram: "16gb", ssd: "512 gb" });
+
+// zunaedsComputer.runComputer();
+// zunaedsComputer.showBrandName();
+// console.log(Computer.motherboardModel);
+
+class Laptop extends Computer {
+    webcam = "25 mp";
+    showCpuName() {
+        console.log(this.cpu);
+    }
+}
+
+let zunaedsLaptop = new Laptop({ cpu: "i5 8500", gpu: "GTX 1650", ram: "16gb", ssd: "512 gb" });
+
+console.log(zunaedsLaptop.brand);
+zunaedsLaptop.showCpuName();
